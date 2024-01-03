0ba349ca38ccf95f207c11da1916cfa03e32c243
Patrick Plasterer <patrickplasterer@gmail.com>
2024-01-03 14:54:44 -0400

    Fixed bug in letter guess.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ words = ['giraffe', 'lion', 'tiger', 'gorilla']
 
 currentWord = words[random.randrange(0,(len(words)-1))]
 
-max_guesses = 3
+max_guesses = 4
 guesses = 0
 
 name = input("What's your name? ")
@@ -15,9 +15,9 @@ result = '_'*len(currentWord)
 
 while True:
     if guesses >= max_guesses:
-        print("Sorry, you ran out of guesses")
+        print(f"Sorry, you ran out of guesses, the word was {currentWord}!")
         break
-    print(f"{name}, the word is {result} and you have {max_guesses - guesses} guesses left")
+    print(f"{name}, the word is {result}, ({currentWord}) and you have {max_guesses - guesses} guesses left")
     state = input("Do you want to guess a letter or the word? (letter/word): ")
     while True:
         if state == 'letter' or state == 'word':
@@ -29,7 +29,7 @@ while True:
         letter = input("Guess a letter: ")
         for i in range(len(currentWord)):
             if letter == currentWord[i]:
-                result.replace(result[i], letter)
+                result = result[:i] + letter + result[i+1:]
         print(result)
         guesses += 1
 
